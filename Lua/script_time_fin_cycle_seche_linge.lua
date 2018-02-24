9a08e1315976185762572bacae4564cdcafc16c0
papo-o <papo-o@users.noreply.github.com>
2018-02-24 11:58:08 +0100

    v1.5 script_time_fin_cycle_seche_linge.lua

--- Lua/script_time_fin_cycle_seche_linge.lua
+++ Lua/script_time_fin_cycle_seche_linge.lua
@@ -1,14 +1,30 @@
---script_time_fin_cycle.lua
+-- script_time_fin_cycle_seche_linge.lua
+-- source : http://www.domoticz.com/forum/viewtopic.php?f=61&t=253
+-- modifié par papoo
+-- MAJ : 24/02/2018
 
---Change the values below to reflect to your own setup
-local washer_status_uservar   = 'Etat Seche Linge'
-local energy_consumption      = 'Conso Sèche Linge'         --Name of Z-Wave plug that contains actual consumption of washingmachine (in Watts)
-local washer_counter_uservar  = 'Compteur Seche Linge'        --Name of the uservariable that will contain the counter that is needed
-local idle_minutes            = 15                               --The amount of minutes the consumption has to stay below the 'consumption_lower' value
-local consumption_upper       = 200                              --If usage is higher than this value (Watts), the washingmachine has started
-local consumption_lower       = 3                             --If usage is lower than this value (Watts), the washingmachine is idle for a moment/done washing
-local name 					  = "Sèche Linge"
-local debugging = false  			-- true pour voir les logs dans la console log Dz ou false pour ne pas les voir
+--------------------------------------------
+------------ Variables à éditer ------------
+--------------------------------------------
+local debugging                 = false  			          -- true pour voir les logs dans la console log Dz ou false pour ne pas les voir 
+local washer_status_uservar     = 'Etat Seche Linge'
+local energy_consumption        = 'Conso Sèche Linge'         -- Nom du device qui mesure la consommation réelle du sèche linge (en watts)
+local washer_counter_uservar    = 'Compteur Seche Linge'      -- Nom de la variable utilisateur qui contiendra la valeur précédente du compteur
+local idle_minutes              = 15                          -- Délai en minutes pendant lequel la consommation devra rester en dessous de la valeur 'consumption_lower' avant notification
+local consumption_upper         = 200                         -- Si l'utilisation est supérieure à cette valeur (Watts), l a démarré
+local consumption_lower         = 3                           -- Si l'utilisation est inférieure à cette valeur (Watts), le sèche linge est inactif
+local name 					    = "Sèche Linge"               -- Nom de l'équipement à transmettre lors de la notification
+local subsystem = "pushbullet"                                -- Les différentes valeurs de subsystem acceptées sont : gcm;http;kodi;lms;nma;prowl;pushalot;pushbullet;pushover;pushsafer;telegram
+                                                              -- pour plusieurs modes de notification séparez chaque mode par un point virgule (exemple : "pushalot;pushbullet"). si subsystem = nil toutes les notifications seront activées.
+--------------------------------------------
+----------- Fin variables à éditer ---------
+--------------------------------------------
+--------------------------------------------
+------------- Autres Variables -------------
+--------------------------------------------
+local nom_script = 'fin cycle Séche linge'
+local version = '1.5'
+
 --------------------------------------------
 ---------------- Fonctions -----------------
 --------------------------------------------   
@@ -17,22 +33,19 @@ require('fonctions_perso')
 --------------------------------------------
 -------------- Fin Fonctions ---------------
 --------------------------------------------
-
--- sWatt, sTotalkWh              = otherdevices_svalues['Clothes Washer Power Usage']:match("([^;]+);([^;]+)")
--- washer_usage                  = tonumber(sWatt)
-washer_usage                  = tonumber(otherdevices_svalues[energy_consumption])
-voir_les_logs('valeur consommmation '.. name ..' = '..washer_usage,debugging)
 commandArray = {}
+voir_les_logs("=========== ".. nom_script .." (v".. version ..") ===========",debugging)
 
-voir_les_logs("script_time_fin_cycle_seche_linge.lua",debugging)
---Virtual switch is off, but consumption is higher than configured level, so washing has started
+washer_usage                  = tonumber(otherdevices_svalues[energy_consumption])
+
+-- Le commutateur virtuel est à l'arrêt, mais la consommation est supérieure au niveau configuré. L'utilisation de l'appareil a donc commencé
 if (washer_usage > consumption_upper) and uservariables[washer_status_uservar] == 0 then
   commandArray['Variable:' .. washer_status_uservar]='1'
   voir_les_logs('La consommation d\'énergie actuelle (' ..washer_usage.. 'W) est au-dessus de la limite supérieure (' ..consumption_upper.. 'W), Donc le '.. name ..' a démarré!',debugging)
   commandArray['Variable:' .. washer_counter_uservar]=tostring(idle_minutes)
 end
 
---Washing machine is not using a lot of energy, check the counter
+-- L'appareil n'utilise pas beaucoup d'énergie, vérification du compteur
 if (washer_usage < consumption_lower) and uservariables[washer_status_uservar] == 1 then 
   commandArray['Variable:' .. washer_counter_uservar]=tostring(math.max(tonumber(uservariables[washer_counter_uservar]) - 1, 0))
   voir_les_logs('La consommation d\'énergie actuelle (' ..washer_usage.. 'W) est au-dessous de la limite inférieure (' ..consumption_lower.. 'W), Le '.. name ..' est au repos ou presque pret',debugging)
@@ -42,12 +55,18 @@ elseif ((uservariables[washer_counter_uservar] ~= idle_minutes) and uservariable
   voir_les_logs('Réinitialisation minuterie '.. name,debugging)
 end
 
---Washingmachine is done
+-- Le cycle est terminé
 if ((uservariables[washer_status_uservar] == 1) and uservariables[washer_counter_uservar] == 0) then
   voir_les_logs('La consommation d\'énergie actuelle du '.. name ..' est de ' ..washer_usage.. 'W',debugging)
   voir_les_logs('Le '.. name ..' vient de se terminer!',debugging)
-  commandArray['SendNotification']='Cycle Terminé: '.. name ..'#Le '.. name ..' vient de se terminer#0'
-  commandArray['Mid Value']='Set Level 2'
+    if subsystem ~= nil then
+        voir_les_logs("--- --- --- Notification système activée pour les services "..subsystem,debugging)
+        commandArray[#commandArray+1] = {['SendNotification'] = 'Cycle Terminé: '.. name ..'#Le '.. name ..' vient de se terminer#0###'.. subsystem ..''}
+    else
+        voir_les_logs("--- --- --- toutes les Notifications système sont activées",debugging)
+        commandArray[#commandArray+1] = {['SendNotification'] = 'Cycle Terminé: '.. name ..'#Le '.. name ..' vient de se terminer'}
+    end
+  commandArray['Mid Value']='Set Level 2' -- notification via xiaomi gateway
   commandArray['Variable:' .. washer_status_uservar]='0'
 end
 
