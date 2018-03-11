2dfa491abb711176b9a41982f07793f568371055
papo-o <papo-o@users.noreply.github.com>
2018-03-11 22:48:32 +0100

    v1.1 script_time_export_emoncms.lua

--- Lua/script_time_export_emoncms.lua
+++ Lua/script_time_export_emoncms.lua
@@ -6,8 +6,8 @@ date : 18/05/2016
 Principe : 
 exporter les données de compteurs, Températures, etc.. sur le site https://emoncms.org/
 Le script est lu toutes les minutes mais n'exporte les données que toutes les 2 minutes
-http://easydomoticz.com/forum/viewtopic.php?f=17&t=2017
-http://pon.fr/exporter-des-donnees-vers-le-site-emoncms/
+https://easydomoticz.com/forum/viewtopic.php?f=17&t=2017
+https://pon.fr/exporter-des-donnees-vers-le-site-emoncms/
 https://github.com/papo-o/domoticz_scripts/blob/master/Lua/script_time_export_emoncms.lua
 ]]--
 
