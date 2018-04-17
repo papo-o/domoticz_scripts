494caf5ad63dbbf2c6cb7506f3e9daa4f634a373
papo-o <papo-o@users.noreply.github.com>
2018-04-17 22:28:10 +0200

    v1.23 script_device_givre_auto.lua

--- Lua/script_time_givre_auto.lua
+++ Lua/script_time_givre_auto.lua
@@ -1,7 +1,7 @@
  
 --[[ ~/domoticz/scripts/lua/script_device_givre_auto.lua
  auteurs : papoo&deennoo
- MAJ : 11/02/2018
+ MAJ : 17/04/2018
  création : 06/05/2016
  Principe : a une heure donnée, va chercher les prévisions de température pour dans 12h puis va calculer le point de givre
  en comparant ensuite le point de givre et la température reçue, création d'une alerte givre.
@@ -20,8 +20,8 @@ la reconnaissance automatique du chemin d'exécution de ce script ne fonctionnant
 
 local debugging = false					    -- true pour voir les logs dans la console log Dz ou false pour ne pas les voir
 local dew_point_idx = nil				    -- idx de l'éventuel dummy température point de rosée si vous souhaitez le suivre
-local freeze_point_idx =19				    -- idx du dummy température point de givre
-local freeze_alert_idx =17				    -- idx du dummy alert point de givre
+local freeze_point_idx = nil				    -- idx du dummy température point de givre
+local freeze_alert_idx = nil				    -- idx du dummy alert point de givre
 local heure = 19						    -- heure de déclenchement
 local minute = 03						    -- minute de déclenchement
 local Longitude = "Longitude"			    -- nom de la variable utilisateur contenant les données de longitude
@@ -31,8 +31,8 @@ local api_forecast_io = "api_forecast_io"	-- nom de la variable utilisateur cont
 --------------------------------------------
 ----------- Fin variables à éditer ---------
 --------------------------------------------
-local nom_script = "prevision et alerte givre"
-local version = "1.22"
+local nom_script = "prévision et alerte givre"
+local version = "1.23"
 local api = ""
 local long = ""
 local lat = ""
@@ -118,25 +118,25 @@ if lat ~= nil then voir_les_logs("--- --- --- Latitude : ".. lat,debugging) end
 
            if(tonumber(temp)<=1 and tonumber(FreezingPoint)<=0)then
                 voir_les_logs("--- Givre Demain Matin ---",debugging)
-                commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|4|'..FreezingPoint}
+                if freeze_alert_idx ~= nil then commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|4|'..FreezingPoint} end
                 commandArray['SendNotification'] = 'Alert#Presence de givre demain matin!'
            elseif(tonumber(temp)<=3 and tonumber(FreezingPoint)<=0)then
                 voir_les_logs("--- Risque de Givre Demain Matin ---",debugging)
-                commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|2|'..FreezingPoint}
+                if freeze_alert_idx ~= nil then commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|2|'..FreezingPoint} end
                 commandArray['SendNotification'] = 'Alert#Risque de givre demain matin!'
             elseif(tonumber(temp)<=0 and tonumber(FreezingPoint)>tonumber(-2)) then
                 voir_les_logs("--- Givre Demain Matin ---",debugging)
-                commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|4|'..FreezingPoint}
+                if freeze_alert_idx ~= nil then commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|4|'..FreezingPoint} end
                 commandArray['SendNotification'] = 'Alert#Presence de givre demain matin!'  
            else
                 voir_les_logs("--- --- --- Aucun risque de Givre --- --- ---",debugging)
-                commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|1|'..'Pas de givre'}
+                if freeze_alert_idx ~= nil then commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|1|'..'Pas de givre'} end
             end 
         else
             voir_les_logs("=========== La temperature du point de rosee est superieure a la temperature. Puisque la temperature du point de rosee ne peut etre superieure a la temperature de l'air , l\'humidite relative a ete fixee à nil ===========",debugging)
             if tonumber(dew)<= -4 then
                 voir_les_logs("--- Risque de Givre Demain Matin ---",debugging)
-                commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|2|'..FreezingPoint}
+                if freeze_alert_idx ~= nil then commandArray[3]={['UpdateDevice'] = freeze_alert_idx..'|2|'..FreezingPoint} end
                 commandArray['SendNotification'] = 'Alert#Risque de givre demain matin!'
             end
         end
