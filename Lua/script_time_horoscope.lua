8a70c31bb3555d42572c0dbd37a3a3b8bff2601f
papo-o <papo-o@users.noreply.github.com>
2018-04-17 23:30:05 +0200

    v1.21 script_time_horoscope.lua

--- Lua/script_time_horoscope.lua
+++ Lua/script_time_horoscope.lua
@@ -18,11 +18,13 @@ local les_horoscopes = {}                   --[[ renseigner le signe choisi en m
                                                  balance, scorpion, sagittaire, capricorne, verseau, poissons ]]--
 les_horoscopes[#les_horoscopes+1] = {device = 'Horoscope Cancer', signe = 'cancer'}
 les_horoscopes[#les_horoscopes+1] = {device = 'Horoscope Capricorne', signe = 'capricorne'}
+
 -- ========================================================================
 -- Fin Variables à éditer
 -- ========================================================================
 local nom_script = 'Horoscope'
-local version = '1.20'
+local version = '1.21'
+local horoscope = ''
 local device = ''
 local signe = ''
 -- ======================================================================== 
@@ -56,7 +58,7 @@ commandArray = {}
 time = os.date("*t")
 if script_actif == true then
     --if (time.hour == 00 and time.min == 20) then -- tout les matins à 00h20
-    if (time.min == 20 and ((time.hour == 7) or (time.hour == 13) or (time.hour == 18))) then -- 3 exécutions du script par jour à 7H20, 13h20 et 18H20
+     if (time.min == 20 and ((time.hour == 7) or (time.hour == 13) or (time.hour == 18))) then -- 3 exécutions du script par jour à 7H20, 13h20 et 18H20
     -- if time.hour % 2 == 0 then -- toutes les deux heures
     -- if (time.hour%1 == 0 and time.min == 10)  then -- Toutes les heures et 10 minutes
     -- if time.min% 2 == 0 then 
@@ -64,22 +66,19 @@ if script_actif == true then
         voir_les_logs("=========== ".. nom_script .." (v".. version ..") ===========",debugging)
         for k,v in pairs(les_horoscopes) do-- On parcourt chaque horoscope
             device = v.device
-            voir_les_logs('--- --- --- traitement de '..device..'  --- --- --',debugging)
+            if device ~= nil and device ~= '' then voir_les_logs('--- --- --- traitement '..device..'  --- --- --',debugging) end
             signe = v.signe
-            voir_les_logs('--- --- --- signe astrologique : '..signe..'  --- --- --',debugging)            
-        local rid = assert(io.popen("/usr/bin/curl -m5 https://astro.rtl.fr/horoscope-jour-gratuit/".. signe))
-        local testhtml = rid:read('*all')
-                rid:close()
-
-            
+            if signe ~= '' then voir_les_logs('--- --- --- signe astrologique : '..signe..'  --- --- --',debugging) end            
+            local rid = assert(io.popen("/usr/bin/curl -m5 https://astro.rtl.fr/horoscope-jour-gratuit/".. signe))
+            local testhtml = rid:read('*all')
+            rid:close()
             for instance in testhtml:gmatch("<body>(.-)</body>") do
                 div, horoscope=instance:match('<h2>En résumé</h2>(.-)<p class="text">(.-)</p>')
             end	
             horoscope = TronquerTexte(horoscope, 240)
-            
-            if horoscope ~= nil  and signe ~= nil and device ~= nil then
+            if horoscope ~= nil  and signe ~= nil then
                 voir_les_logs("--- --- --- ".. signe .." : ".. horoscope,debugging)
-                if device ~= nil then
+                if device ~= nil and device ~= '' then
                     commandArray[#commandArray+1] = {['UpdateDevice'] = otherdevices_idx[device]..'|0|'.. horoscope}
                 end
                 if send_notification > 0 then
