238776970d9d5c07fefac8aea10034502334af9b
papo-o <papo-o@users.noreply.github.com>
2018-04-28 23:26:27 +0200

    v0.02 script_time_action_sur_luminosite.lua

--- Lua/script_time_action_sur_luminosite.lua
+++ Lua/script_time_action_sur_luminosite.lua
@@ -1,7 +1,7 @@
 --[[   
-~/domoticz/scripts/lua/script_time_test.lua
+~/domoticz/scripts/lua/script_time_action_sur_luminosite.lua
 auteur : papoo
-MAJ : 02/01/2018
+MAJ : 28/04/2018
 création : 02/01/2018
 Principe :
 ]]--
@@ -13,30 +13,33 @@ local lum ='Luminosité Salon'
 local seuil = 2
 local lum_rdc_1 = 'lumiere_rdc_3'
 local delai_apres_leve_soleil = tonumber(0) -- delai en minutes 
-local delai_apres_couche_soleil = tonumber(0) -- delai en minutes 
+local delai_apres_couche_soleil = tonumber(60) -- delai en minutes 
 --------------------------------------------
 ----------- Fin variables à éditer ---------
 -------------------------------------------- 
 commandArray = {}
 local nom_script = 'Action sur seuil luminosité'
-local version = '0.01'
-
+local version = '0.02'
 time=os.date("*t")
 --------------------------------------------
 ---------------- Fonctions -----------------
 -------------------------------------------- 
 curl = '/usr/bin/curl -m 15 -u domoticzUSER:domoticzPSWD '
---==============================================================================================
-function voir_les_logs (s, debugging)
+--------------------------------------------
+package.path = package.path..";/home/pi/domoticz/scripts/lua/fonctions/?.lua"   -- ligne à commenter en cas d'utilisation des fonctions directement dans ce script
+require('fonctions_perso')                                                      -- ligne à commenter en cas d'utilisation des fonctions directement dans ce script
+
+-- ci-dessous les lignes à décommenter en cas d'utilisation des fonctions directement dans ce script( supprimer --[[ et --]])
+--[[function voir_les_logs (s, debugging) -- nécessite la variable local debugging
     if (debugging) then 
 		if s ~= nil then
-        print ("<font color='#f3031d'>".. s .."</font>");
+        print (s)
 		else
-		print ("<font color='#f3031d'>aucune valeur affichable</font>");
+		print ("aucune valeur affichable")
 		end
     end
-end	
---==============================================================================================
+end	-- usage voir_les_logs("=========== ".. nom_script .." (v".. version ..") ===========",debugging)
+--------------------------------------------
 function round(value, digits)
 	if not value or not digits then
 		return nil
@@ -46,12 +49,13 @@ function round(value, digits)
 		  (math.floor(value * precision + 0.5) / precision) or
 		  (math.ceil(value * precision - 0.5) / precision)
 end
---==============================================================================================
+--]]
+--------------------------------------------
 function calc_dju(temperature)
 dju  = tonumber((18 - temperature)*1/1440)
 return dju
 end
---============================================================================================== 
+-------------------------------------------- 
 function url_encode(str) -- encode la chaine str pour la passer dans une url 
    if (str) then
    str = string.gsub (str, "\n", "\r\n")
@@ -61,11 +65,11 @@ function url_encode(str) -- encode la chaine str pour la passer dans une url
    end
    return str
 end
---============================================================================================== 
+-------------------------------------------- 
 function creaVar(vname,vtype,vvalue) -- pour créer une variable de type 2 nommée toto comprenant la valeur 10
 	os.execute(curl..'"'.. domoticzURL ..'/json.htm?type=command&param=saveuservariable&vname='..url_encode(vname)..'&vtype='..vtype..'&vvalue='..url_encode(vvalue)..'" &')
 end -- usage :  creaVar('toto','2','10')  
---==============================================================================================
+--------------------------------------------
 -- Obtenir le nom d'un device via son idx
 function GetDeviceNameByIDX(deviceIDX) -- https://www.domoticz.com/forum/viewtopic.php?t=18736#p144720
     deviceIDX = tonumber(deviceIDX)
@@ -76,7 +80,7 @@ function GetDeviceNameByIDX(deviceIDX) -- https://www.domoticz.com/forum/viewtop
    end
    return 0
 end -- exemple usage = commandArray[GetDeviceNameByIDX(383)] = 'On'
---==============================================================================================
+--------------------------------------------
 -- Obtenir l'idx d'un device via son nom
 function GetDeviceIdxByName(deviceName) 
    for i, v in pairs(otherdevices_idx) do
@@ -86,7 +90,7 @@ function GetDeviceIdxByName(deviceName)
    end
    return 0
 end -- exemple usage = commandArray['UpdateDevice'] = GetDeviceIdxByName('Compteur Gaz') .. '|0|' .. variable
---==============================================================================================
+--------------------------------------------
 -- Function to strip charachters
 function
  stripchars(str, chrs)
