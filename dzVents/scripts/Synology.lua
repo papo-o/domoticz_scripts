1b83eca0a33edcafdc7b0a0487c4ea761df4bc88
papo-o <papo-o@users.noreply.github.com>
2018-09-03 21:25:52 +0200

    v1.04 Synology.lua

--- dzVents/scripts/Synology.lua
+++ dzVents/scripts/Synology.lua
@@ -2,7 +2,7 @@
 name : synology.lua
 auteur : papoo
 creation : 24/08/2018
-mise à  jour : 28/08/2018
+mise à  jour : 03/09/2018
 
 https://pon.fr/dzvents-supervision-dun-nas-synology-avec-snmp/
 https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/Synology.lua
@@ -29,12 +29,20 @@ Reboot your Pi:
 sudo reboot
 
 Check if SNMP is up and running, issue:
-snmpget -v 2c -c PASSWORD -O qv NASIPADDRESS 1.3.6.1.4.1.24681.1.2.11.1.3.1
+snmpget -v 2c -c PASSWORD -O qv NASIPADDRESS 1.3.6.1.4.1.6574.1.5.1.0
 
 Replace PASSWORD with the Community name you entered while setting up your NAS
 Replace NASIPADDRESS with the ip address of your NAS
 You should get something like this:
-"37" which corresponds to the temperature of disc # 1
+"DS218+" which corresponds to your Synology Model
+
+test 
+snmpget -v 2c -c PASSWORD -O qv NASIPADDRESS 1.3.6.1.2.1.25.2.3.1.5.31
+snmpget -v 2c -c PASSWORD -O qv NASIPADDRESS 1.3.6.1.2.1.25.2.3.1.5.38
+snmpget -v 2c -c PASSWORD -O qv NASIPADDRESS 1.3.6.1.2.1.25.2.3.1.5.41
+then modify     OID_HDUnit, OID_HDTotal, OID_HDUsed variables (.38 on DSM 5.1, .41 on DSM 6.0+ or .31 on my new DS218)
+
+if you have à DS2XX model, uncomment     -- local OID_HDtemp2='1.3.6.1.4.1.6574.2.1.1.6.1'  to show the hdd2 temperature
 Then create:
 1 device Switch
 1 temperature device
@@ -58,7 +66,7 @@ end
 -------------------------------------------
 
     local NasIp = "192.168.100.250"                             -- NAS IP Address
-    local CommunityPassword = "synology"                        -- SNMP Password
+    local CommunityPassword = "private"                         -- SNMP Password
     local NAS = "Synology"                                      -- NAS Switch
     local NAS_HD1_TEMP = "Synology Temp"                        -- NAS HD1 Temp => 
     local NAS_CPU = "Synology Utilisation CPU"                  -- NAS CPU 
@@ -67,9 +75,9 @@ end
     local NAS_HD_SPACE = "Synology Espace Disponible"           -- NAS HD Space  in Go
     local NAS_HD2_TEMP = ""                                     -- NAS HD2 Temp 
     local OID_HDtemp1 = '1.3.6.1.4.1.6574.2.1.1.6.0'            -- Temperature HD1
-    local OID_HDUnit = '1.3.6.1.2.1.25.2.3.1.4.41'              -- OID HD Unit Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ 
-    local OID_HDTotal = '1.3.6.1.2.1.25.2.3.1.5.41'             -- OID Total space volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ 
-    local OID_HDUsed = '1.3.6.1.2.1.25.2.3.1.6.41'              -- OID Space used volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+
+    local OID_HDUnit = '1.3.6.1.2.1.25.2.3.1.4.41'              -- OID HD Unit Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .31 on my new DS218
+    local OID_HDTotal = '1.3.6.1.2.1.25.2.3.1.5.41'             -- OID Total space volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .31 on my new DS218
+    local OID_HDUsed = '1.3.6.1.2.1.25.2.3.1.6.41'              -- OID Space used volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+or .31 on my new DS218
     local OID_CpuUser = '1.3.6.1.4.1.2021.11.9.0'               -- OID CPU user
     local OID_CpuSystem = '1.3.6.1.4.1.2021.11.10.0'            -- OID CPU System
     local OID_MemAvailable = '1.3.6.1.4.1.2021.4.13.0'          -- OID Free Memory Available
@@ -85,7 +93,7 @@ return {
                 level    =   domoticz.LOG_ERROR,                                            -- Only one level can be active; comment others
                 -- level    =   domoticz.LOG_DEBUG,
                 -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
-                marker    =   "Synology Monitor v1.02 "      },
+                marker    =   "Synology Monitor v1.04 "      },
     
     execute = function(dz)
         local i = 0
@@ -109,8 +117,10 @@ return {
         if results[4] then dz.log("CpuUser : "..results[4],dz.LOG_DEBUG) end      
         if results[5] then dz.log("CpuSystem : "..results[5],dz.LOG_DEBUG) end  
         if results[6] then dz.log("MemAvailable : "..results[6],dz.LOG_DEBUG) end
-        if results[7] then dz.log("HDTemp2 : "..results[7],dz.LOG_DEBUG) end
-        
+        if results[7] then
+            dz.log("HDTemp2 : "..results[0],dz.LOG_DEBUG)
+            if NAS_HD2_TEMP then dz.devices(NAS_HD2_TEMP).update(0,results[0]) end
+            end        
         local HDFree = dz.utils.round(((results[2] - results[3]) *  results[1] / 1024 / 1024 / 1024), 0)
         if HDFree then 
             dz.log("HDFree : "..HDFree,dz.LOG_DEBUG) 
@@ -126,7 +136,7 @@ return {
             dz.log("CpuUsed : "..CpuUsed,dz.LOG_DEBUG)
             dz.devices(NAS_CPU).update(0,CpuUsed)
             end
-        local MemUsedPerc = dz.utils.round(((results[6] / 1024)), 0)
+        local MemUsedPerc = dz.utils.round((((results[6] / 1024) *100 / 1024)), 0)
         if MemUsedPerc then 
             dz.log("MemUsedPercent : "..MemUsedPerc,dz.LOG_DEBUG)
             dz.devices(NAS_MEM).update(0,MemUsedPerc)
