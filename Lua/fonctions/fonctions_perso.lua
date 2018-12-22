6712a758978e018effd842eba4724173fa47f150
papo-o <papo-o@users.noreply.github.com>
2018-12-22 13:19:41 +0100

    v1.6 fonctions_perso.lua

--- Lua/fonctions/fonctions_perso.lua
+++ Lua/fonctions/fonctions_perso.lua
@@ -1,5 +1,5 @@
 --[[ 
-version 1.55
+version 1.6
 appel de ces fonctions :
 package.path = package.path..";/home/pi/domoticz/scripts/lua/fonctions/?.lua"
 ou
@@ -684,6 +684,203 @@ function date_en_francais(str)
      end
     return (str)
 end
+
+---------------------------------------
+function rand(Table) -- retourne un élément d'une table au hasard
+    t = Table[math.random(1, #Table)]
+    return t
+end
+
+
+---------------------------------------
+--- functions sur les tables---source https://love2d.org/forums/viewtopic.php?t=85140
+--------------------------------------------
+
+--- Checks if the table is empty
+-- @param t Table
+-- @return True if empty or false otherwise
+local _cache = {}
+function empty_table(t)
+  return next(t) == nil
+end
+
+--- Removes all values
+-- @param t Table
+function clear_table(t)
+  for i in pairs(t) do
+    t[i] = nil
+  end
+end
+
+--- Counts the number of elements
+-- @param t Table
+-- @return Total number of elements
+function count_table(t)
+  local n = 0
+  for _ in pairs(t) do
+    n = n + 1
+  end
+  return n
+end
+
+--- Counts the number of unique elements
+--- The number of duplicate elements can be calculated
+--- by subtracting the first return value from the second
+-- @param t Table
+-- @return Number of unique values
+-- @return Total number of elements
+function ucount_table(t)
+  -- clear the cache
+  clear_table(_cache)
+  local n = 0
+  local d = 0
+  for _, v in pairs(t) do
+    if _cache[v] then
+      d = d + 1
+    else
+      _cache[v] = true
+    end
+    n = n + 1
+  end
+  return n - d, n
+end
+
+--- Counts the number of occurrences of a given value
+-- @param t Table
+-- @param v Value
+-- @return Number of occurrences
+-- @return Total number of elements
+function vcount_table(t, s)
+  assert(s ~= nil, "second argument cannot be nil")
+  local n = 0
+  local d = 0
+  for _, v in pairs(t) do
+    if v == s then
+      d = d + 1
+    end
+    n = n + 1
+  end
+  return d, n
+end
+
+--- Reverses the elements in a list
+-- @param t Table
+-- @param r Destination table (optional)
+-- @return Reversed table
+function reverse_table(t, r)
+  r = r or t
+  local n = #t
+  if t == r then
+    -- reverse in place
+    for i = 1, n/2 do
+      local i2 = n - i + 1
+      r[i], r[i2] = r[i2], r[i]
+    end
+  else
+    -- reverse copy
+    for i = 1, n do
+      r[n - i + 1] = t[i]
+    end
+  end
+  return r
+end
+
+--- Shuffles the elements in a list
+-- @param t Table
+-- @param r Destination table (optional)
+-- @return Reversed table
+local rand = math.random
+function shuffle_table(t, r)
+  r = r or t
+  local n = #t
+  -- shuffle copy
+  if t ~= r then
+    for i = 1, n do
+      r[i] = t[i]
+    end
+  end
+  -- shuffle in place
+  for i = n, 1, -1 do
+    local j = rand(n)
+    r[i], r[j] = r[j], r[i]
+  end
+  return r
+end
+
+--- Finds the first occurrence in a list
+-- @param t Table
+-- @param s Search value
+-- @param o Starting index (optional)
+-- @return Numeric index or nil
+function find_table(t, s, o)
+  o = o or 1
+  assert(s ~= nil, "second argument cannot be nil")
+  for i = o, #t do
+    if t[i] == s then
+      return i
+    end
+  end
+end
+
+--- Finds the last occurrence in a list
+-- @param t Table
+-- @param s Search value
+-- @param o Starting index (optional)
+-- @return Numeric index or nil
+function rfind_table(t, s, o)
+  o = o or #t
+  assert(s ~= nil, "second argument cannot be nil")
+  -- iterate in reverse
+  for i = o, 1, -1 do
+    if t[i] == s then
+      return i
+    end
+  end
+end
+
+--- Recursive deep copy (internal)
+--- "cache" must be empty prior to calling this function
+-- @param s Source table
+-- @param d Destination table
+local function dcopy_table(s, d)
+  -- copy elements from the source table
+  for k, v in pairs(s) do
+    if type(v) == "table" then
+      if _cache[v] then
+        -- reference cycle
+        d[k] = _cache[v]
+      else
+        -- recursive copy
+        local d2 = d[k]
+        if d2 == nil then
+          d2 = {}
+          d[k] = d2
+        end
+        _cache[v] = d2
+        dcopy(v, d2)
+      end
+    else
+      d[k] = v
+    end
+  end
+end
+
+--- Copies the contents from one table to another
+--- Overwrites existing elements in the destination table
+--- Preserves table cycles
+-- @param s Source table
+-- @param d Destination table (optional)
+-- @return Resulting table
+function copy_table(s, d)
+  d = d or {}
+  assert(s ~= d, "source and destination tables must be different")
+  -- clear the cache
+  clear_table(_cache)
+  -- deep copy
+  dcopy(s, d)
+  return d
+end
+
 -------------------------------------------
 -------------Fin Fonctions-----------------
 -------------------------------------------
