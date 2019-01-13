2896ba79f264675623b1870899058682b66f1c77
papo-o <papo-o@users.noreply.github.com>
2019-01-13 20:09:33 +0100

    v1.2 twitterFollowers.lua

--- dzVents/scripts/twitterFollowers.lua
+++ dzVents/scripts/twitterFollowers.lua
@@ -5,20 +5,19 @@ update/mise à jour = 10/01/2019
 creation = 10/01/2019
 https://pon.fr/dzvents-suivre-le-nombre-dabonnes-dun-compte-twitter
 https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/twitterFollowers.lua
-https://easydomoticz.com/forum/viewtopic.php?f=17&t=7809
-https://www.domoticz.com/forum/viewtopic.php?f=59&t=26482
 --]]
 --------------------------------------------
 ------------ Variables à éditer ------------
 --------------------------------------------
 local twitterName = 'pap_oo'
-local deviceTwitter = '@pap_oo'         -- nom (entre ' ') ou idx du custom device twitter, nil si non utilisé
-local intervalleMiseAJourEnMinutes = 30   -- intervalle de mise à jour du script.
+local deviceTwitter = '@pap_oo'              -- nom (entre ' ') ou idx du custom device twitter, nil si non utilisé
+local intervalleMiseAJourEnMinutes = 30     -- intervalle de mise à jour du script en minutes.
+local rafraichissementDevice = 1            -- intervalle de mise à jour du custom device twitter en jour.         
 --------------------------------------------
 ----------- Fin variables à éditer ---------
 --------------------------------------------
 local nomScript = 'nombre de Followers sur twitter'
-local versionScript = '1.0'
+local versionScript = '1.2'
 
 return {
     active = true,
@@ -27,7 +26,7 @@ return {
                     httpResponses   =   { nomScript }    -- Trigger the handle Json part
                 },
 
-  logging =   { level    =   domoticz.LOG_INFO,
+  logging =   { --level    =   domoticz.LOG_INFO,
                 marker = nomScript..' '..versionScript
                },
 
@@ -51,7 +50,10 @@ return {
                json = domoticz.utils.fromJSON(reponse.data)
                domoticz.log(tostring(json.name)..' à '..tostring(json.followers_count)..' followers sur twitter sous le pseudo de '..tostring(json.screen_name),domoticz.LOG_INFO)
                if json.followers_count and deviceTwitter then
-                    domoticz.devices(deviceTwitter).updateCustomSensor(tonumber(json.followers_count))
+                   if tonumber(json.followers_count) > tonumber(domoticz.devices(deviceTwitter)._state) or tonumber(domoticz.devices(deviceTwitter).lastUpdate.daysAgo) >= tonumber(rafraichissementDevice) then
+                       domoticz.devices(deviceTwitter).updateCustomSensor(tonumber(json.followers_count))
+                       domoticz.log('Mise  à jour du device '..deviceTwitter..' à '..tostring(json.followers_count),domoticz.LOG_INFO)
+                   end
                end
             end
    
