edc65312558589b5355e53b22db02a4e022ca8dd
papo-o <papo-o@users.noreply.github.com>
2019-04-05 13:27:08 +0200

    v1.35 Synology2.lua
    Amélioration du calcul de la mémoire utilisée et prise en compte de la température HDD jusqu’à 4 disques

--- dzVents/scripts/synology2.lua
+++ dzVents/scripts/synology2.lua
@@ -2,7 +2,7 @@
 name : synology2.lua
 auteur : papoo
 creation : 17/02/2019
-mise à  jour : 03/09/2018
+mise à  jour : 05/04/2019
 
 https://pon.fr/dzvents-supervision-dun-nas-synology-avec-snmp/
 https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/synology2.lua
@@ -82,29 +82,41 @@ end
 -------------Fin Fonctions-----------------
 -------------------------------------------
 
-    local NasIp = "diskstation2"                                -- NAS IP Address
-    local CommunityPassword = "synology"                        -- SNMP Password
-    local NAS = "Synology"                                      -- NAS Switch
+    local NasIp = "diskstation2"                            -- NAS IP Address
+    local CommunityPassword = "synology"                    -- SNMP Password
+    local NAS = "Synology"                                  -- NAS Switch
     local NAS_TEMP = "Synology Temp"
-    local NAS_CPU = "Synology Utilisation CPU"                  -- NAS CPU 
-    local NAS_MEM = "Synology Utilisation RAM"                  -- NAS MEM 
-    local NAS_HD_SPACE_PERC = "Synology Occupation Disque"      -- NAS HD Space  in %
-    local NAS_HD_SPACE = "Synology Espace Disponible"           -- NAS HD Space  in Go
-    local NAS_HDD1_TEMP = "Synology HDD1 Temp"                   -- NAS HD1 Temp 
-    -- local NAS_HDD2_TEMP = ""                                     -- NAS HD2 Temp 
+    local NAS_CPU = "Synology Utilisation CPU"              -- NAS CPU 
+    local NAS_MEM = "Synology Utilisation RAM"              -- NAS MEM 
+    local NAS_HD_SPACE_PERC = "Synology Occupation Disque"  -- NAS HD Space  in %
+    local NAS_HD_SPACE = "Synology Espace Disponible"       -- NAS HD Space  in Go (custom sensor)
+    local NAS_HDD1_TEMP = "Synology HDD1 Temp"              -- NAS HD1 Temp, nil si inutilisé 
+    local NAS_HDD2_TEMP = nil                               -- NAS HD2 Temp, nil si inutilisé
+    local NAS_HDD3_TEMP = nil                               -- NAS HD3 Temp, nil si inutilisé
+    local NAS_HDD4_TEMP = nil                               -- NAS HD4 Temp, nil si inutilisé
     local OID_NAS_TEMP = '1.3.6.1.4.1.6574.1.2.0'
-    local OID_HDUnit = '1.3.6.1.2.1.25.2.3.1.4.51'              -- OID HD Unit Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
-    local OID_HDTotal = '1.3.6.1.2.1.25.2.3.1.5.51'             -- OID Total space volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
-    local OID_HDUsed = '1.3.6.1.2.1.25.2.3.1.6.51'              -- OID Space used volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
-    local OID_CpuUser = '1.3.6.1.4.1.2021.11.9.0'               -- OID CPU user
-    local OID_CpuSystem = '1.3.6.1.4.1.2021.11.10.0'            -- OID CPU System
-    local OID_MemAvailable = '1.3.6.1.4.1.2021.4.13.0'          -- OID Free Memory Available
-    local OID_HDtemp1 = '1.3.6.1.4.1.6574.2.1.1.6.0'            -- OID Temperature HD1   
-    local OID_HDtemp2 = '1.3.6.1.4.1.6574.2.1.1.6.1'            -- OID Temperature HD2 
-    local OID_Raid_Status = '1.3.6.1.4.1.6574.3.1.1.3.0'        -- OID Raid Status
-    local OID_Physical_Memory_Units = '1.3.6.1.2.1.25.2.3.1.4.1'
-    local OID_Physical_Memory_Size = '1.3.6.1.2.1.25.2.3.1.5.1'
-    local OID_Physical_Memory_Used = '1.3.6.1.2.1.25.2.3.1.6.1'     
+    local OID_HDUnit = '1.3.6.1.2.1.25.2.3.1.4.51'          -- OID HD Unit Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
+    local OID_HDTotal = '1.3.6.1.2.1.25.2.3.1.5.51'         -- OID Total space volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
+    local OID_HDUsed = '1.3.6.1.2.1.25.2.3.1.6.51'          -- OID Space used volume in Go Change OID to .38 on DSM 5.1 or .41 on DSM 6.0+ or .51 on my DS218
+    local OID_CpuUser = '1.3.6.1.4.1.2021.11.9.0'           -- OID CPU user
+    local OID_CpuSystem = '1.3.6.1.4.1.2021.11.10.0'        -- OID CPU System
+    local OID_MemAvailable = '1.3.6.1.4.1.2021.4.13.0'      -- OID Free Memory Available
+    local OID_MemTotalSwap = '1.3.6.1.4.1.2021.4.3.0'       -- OID MemTotalSwap
+    local OID_MemTotalReal = '1.3.6.1.4.1.2021.4.5.0'       -- OID MemTotalReal
+    local OID_MemTotalFree = '1.3.6.1.4.1.2021.4.11.0'      -- OID MemTotalFree
+    local OID_HDtemp1 = '1.3.6.1.4.1.6574.2.1.1.6.0'        -- OID Temperature HDD1   
+    local OID_HDtemp2 = '1.3.6.1.4.1.6574.2.1.1.6.1'        -- OID Temperature HDD2 
+    local OID_HDtemp3 = '1.3.6.1.4.1.6574.2.1.1.6.2'        -- OID Temperature HDD3
+    local OID_HDtemp4 = '1.3.6.1.4.1.6574.2.1.1.6.3'        -- OID Temperature HDD4
+    --local OID_Raid_Status = '1.3.6.1.4.1.6574.3.1.1.3.0'        -- OID Raid Status
+    --local OID_Physical_Memory_Units = '1.3.6.1.2.1.25.2.3.1.4.1'
+    --local OID_Physical_Memory_Size = '1.3.6.1.2.1.25.2.3.1.5.1'
+    --local OID_Physical_Memory_Used = '1.3.6.1.2.1.25.2.3.1.6.1'
+    --local OID_system_memory_total = '1.3.6.1.4.1.2021.4.5.0'
+    --local OID_system_memory_free = '1.3.6.1.4.1.2021.4.6.0'
+    --local OID_system_vsmemory_shared = '1.3.6.1.4.1.2021.4.13.0'
+    --local OID_system_vsmemory_buffer = '1.3.6.1.4.1.2021.4.14.0'
+
    
 return {
     active = true,
@@ -113,20 +125,18 @@ return {
     },    
    -- on = { devices = { "your trigger device" }},
         
-  logging =   { -- level    =   domoticz.LOG_INFO,                                             -- Seulement un niveau peut être actif; commenter les autres
-                -- level    =   domoticz.LOG_ERROR,                                            -- Only one level can be active; comment others
-                -- level    =   domoticz.LOG_DEBUG,
+  logging =   {   level    =   domoticz.LOG_DEBUG,                                             -- Seulement un niveau peut être actif; commenter les autres
+                -- level    =   domoticz.LOG_INFO,                                            -- Only one level can be active; comment others
+                -- level    =   domoticz.LOG_ERROR,
                 -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
-                marker    =   "Synology Monitor v1.2 "      },
+                marker    =   "Synology Monitor v1.35 "      },
     
     execute = function(dz)
         local i = 0
         local results = {}
-        local command = 'snmpget -v 2c -c '..CommunityPassword..' -O qv '..NasIp..' '..OID_NAS_TEMP..' '..OID_HDUnit..' '..OID_HDTotal..' '..OID_HDUsed..' '..OID_CpuUser..' '..OID_CpuSystem..' '..OID_MemAvailable..' '..OID_HDtemp1..' '..OID_HDtemp2 --..' '..OID_Physical_Memory_Units..' '..OID_Physical_Memory_Used..' '..OID_Physical_Memory_Size
+        local command = 'snmpget -v 2c -c '..CommunityPassword..' -O qv '..NasIp..' '..OID_NAS_TEMP..' '..OID_HDUnit..' '..OID_HDTotal..' '..OID_HDUsed..' '..OID_CpuUser..' '..OID_CpuSystem..' '..OID_MemAvailable ..' '.. OID_MemTotalSwap ..' '..OID_MemTotalReal..' '..OID_MemTotalFree..' '..OID_HDtemp1..' '..OID_HDtemp2..' '..OID_HDtemp3..' '..OID_HDtemp4--..' '..OID_Physical_Memory_Units ..' '..OID_Physical_Memory_Used..' '..OID_Physical_Memory_Size ..' '..OID_system_memory_total ..' '..OID_system_memory_free ..' '.. OID_system_vsmemory_shared ..' '.. OID_system_vsmemory_buffer 
         local handle = assert(io.popen(command))
         for line in handle:lines() do
-        --for i, line in pairs(handle) do
-            --dz.log(format(line).." "..i,dz.LOG_info)
             results[i] =  format(line)
             i = i + 1
         end
@@ -135,39 +145,23 @@ return {
             dz.log("NASTemp : "..results[0],dz.LOG_DEBUG)
             if NAS_TEMP then dz.devices(NAS_TEMP).update(0,results[0]) end
             end
-        if results[1] then dz.log("HDUnit : "..results[1],dz.LOG_DEBUG) end        
-        if results[2] then dz.log("HDTotal : "..results[2],dz.LOG_DEBUG) end        
+        if results[1] then dz.log("HDUnit : "..results[1],dz.LOG_DEBUG) end
+        if results[2] then dz.log("HDTotal : "..results[2],dz.LOG_DEBUG) end
         if results[3] then dz.log("HDUsed : "..results[3],dz.LOG_DEBUG) end
-        if results[4] then dz.log("CpuUser : "..results[4],dz.LOG_DEBUG) end      
-        if results[5] then dz.log("CpuSystem : "..results[5],dz.LOG_DEBUG) end  
-        if results[6] then dz.log("MemAvailable : "..results[6],dz.LOG_DEBUG) end
-        if results[7] then 
-            dz.log("HDDTemp1 : "..results[7],dz.LOG_DEBUG) 
-            if NAS_HDD1_TEMP then dz.devices(NAS_HDD1_TEMP).update(0,results[7]) end
-            end        
-        if results[8] then 
-            dz.log("HDDTemp2 : "..results[8],dz.LOG_DEBUG) 
-            if NAS_HDD2_TEMP then dz.devices(NAS_HDD2_TEMP).update(0,results[8]) end
-            end
-        -- if results[9] then dz.log("Physical Memory Units : "..results[9],dz.LOG_DEBUG) end
-        -- if results[10] then dz.log("Physical Memory Used : "..results[10],dz.LOG_DEBUG) end
-        -- if results[11] then dz.log("Physical Memory Size : "..results[11],dz.LOG_DEBUG) end
+        if results[4] then dz.log("CpuUser : "..results[4],dz.LOG_DEBUG) end
+        if results[5] then dz.log("CpuSystem : "..results[5],dz.LOG_DEBUG) end
         
         if results[1] and results[2] then
-           --local  HDFree = dz.utils.round(((results[2] - results[3]) *  results[1] / 1024 / 1024 / 1024), 1)
+
            HDTotalGo = dz.utils.round((results[2] *  results[1] / 1024 / 1024 / 1024/1024),2)
            dz.log("HDTotalGo : "..HDTotalGo,dz.LOG_DEBUG)
         end
          if results[1] and results[3] then
            HDUsedGo = dz.utils.round(((results[3]) *  results[1] / 1024 / 1024 / 1024/1024), 2)
-           --local  HDUsedGo = (results[3] *  results[1] / 1024 / 1024 / 1024)
-           --dz.log("HDUsedGo : "..HDUsedGo,dz.LOG_DEBUG)
         end       
-        
+
         if results[1] and results[2] and results[3] then
-           --local  HDFree = dz.utils.round(((results[2] - results[3]) *  results[1] / 1024 / 1024 / 1024), 1)
             HDFreeGo = dz.utils.round(((results[2] - results[3]) *  results[1] / 1024 / 1024 / 1024/1024),2)
-           --dz.log("HDFreeGo : "..HDFreeGo,dz.LOG_DEBUG)
         end
         if HDFreeGo then 
             dz.log("HDFreeGo : "..HDFreeGo,dz.LOG_DEBUG) 
@@ -187,24 +181,42 @@ return {
             dz.log("CpuUsed : "..CpuUsed,dz.LOG_DEBUG)
             dz.devices(NAS_CPU).update(0,CpuUsed)
             end
-        if results[6] then
-            MemUsedPerc = dz.utils.round((((results[6] / 1024) *100 / 1024)), 0)
+
+        if results[7] then
+            dz.log("memTotalSwap: "..tostring(results[7]),dz.LOG_DEBUG)
+        end
+        if results[8] then
+            dz.log("memTotalReal: "..tostring(results[8]),dz.LOG_DEBUG)
+        end
+        if results[9] then
+            dz.log("memTotalFree: "..tostring(results[9]),dz.LOG_DEBUG)
+        end
+
+        if results[7] and results[8] and results[9] then
+            MemUsedPerc = dz.utils.round(100-results[9]*100/(results[7]+results[8]), 0)
         end
         if MemUsedPerc then 
             dz.log("MemUsedPercent : "..MemUsedPerc,dz.LOG_DEBUG)
             dz.devices(NAS_MEM).update(0,MemUsedPerc)
-            end
-        -- if results[9] and results[11] then
-            -- MemTotal = dz.utils.round((((results[11] / results[9]) *100 / 1024)), 0)
-        -- end
-        -- if MemTotal then 
-            -- dz.log("MemTotal : "..MemTotal,dz.LOG_DEBUG)
-            dz.devices(NAS_MEM).update(0,MemUsedPerc)
-            -- end            
-            
-            
-            
-            
+        end
+
+        if results[10] then 
+            dz.log("HDTemp1 : "..tostring(results[10]),dz.LOG_DEBUG) 
+            if NAS_HDD1_TEMP ~= nil then dz.devices(NAS_HDD1_TEMP).update(0,results[10]) end
+        end
+        if results[11] then 
+            dz.log("HDTemp2 : "..tostring(results[11]),dz.LOG_DEBUG) 
+            if NAS_HDD2_TEMP ~= nil then dz.devices(NAS_HDD2_TEMP).update(0,results[11]) end
+        end
+        if results[12] then 
+            dz.log("HDTemp3 : "..tostring(results[12]),dz.LOG_DEBUG) 
+            if NAS_HDD3_TEMP ~= nil then dz.devices(NAS_HDD3_TEMP).update(0,results[12]) end
+        end
+        if results[13] then 
+            dz.log("HDTemp4 : "..tostring(results[13]),dz.LOG_DEBUG) 
+            if NAS_HDD4_TEMP ~= nil then dz.devices(NAS_HDD4_TEMP).update(0,results[13]) end
+        end
+
         if results[0] and results[5] and results[6] then
                 dz.log("Requete SNMP correcte ",dz.LOG_DEBUG)
                 dz.devices(NAS).switchOn().checkFirst()
@@ -212,9 +224,7 @@ return {
                 dz.log("Requete SNMP incorrecte ",dz.LOG_DEBUG)
                 dz.devices(NAS).switchOff().checkFirst()
             end
-            
 
-            
         --RAID Status :
         --OK(1), Repairing(2), Migrating(3), Expanding(4), Deleting(5), Creating(6), RaidSyncing(7), RaidParityChecking(8), RaidAssembling(9), Canceling(10), Degraded(11), Creashed(12)
     end
