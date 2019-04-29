4ba3ffe369bf4b6278977ced8849b1dd1a9818ca
papo-o <papo-o@users.noreply.github.com>
2019-04-29 14:19:20 +0200

    v1.1 xiaomiTHBsensors
    gestion des devices Temp + hygro (même non xiaomi)

--- dzVents/scripts/xiaomiTHBsensors.lua
+++ dzVents/scripts/xiaomiTHBsensors.lua
@@ -1,7 +1,7 @@
 --[[
 xiaomiTHBsensors.lua
 author/auteur = papoo
-update/mise à jour = 26/04/2019
+update/mise à jour = 29/04/2019
 création = 22/04/2019
 https://pon.fr/dzvents-gestion-des-sondes-xiaomi-thb-avec-le-plugin-domoticz-deconz
 https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/xiaomiTHBsensors.lua
@@ -10,12 +10,12 @@ https://easydomoticz.com/forum/viewtopic.php?f=15&t=7593&p=69089#p69089
 
 
 local scriptName = 'xiaomiTHBsensors'
-local scriptVersion = '1.0'
+local scriptVersion = '1.1'
 
 return {
     active = true,
     logging = {
-                    -- level    =   domoticz.LOG_DEBUG, -- Uncomment to override the dzVents global logging setting
+                     level    =   domoticz.LOG_DEBUG, -- Uncomment to override the dzVents global logging setting
                     -- level    =   domoticz.LOG_INFO,  -- Seulement un niveau peut être actif; commenter les autres
                     -- level    =   domoticz.LOG_ERROR,
                     -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
@@ -31,7 +31,7 @@ return {
         end
         local function levelForecast(pressure)
             pressure = tonumber(pressure)
-            logWrite(tostring(type(pressure)), domoticz.LOG_DEBUG)
+            logWrite(type(pressure))
             if type(pressure) ~= "number" then
                 return domoticz.BARO_NOINFO
             elseif pressure < 1000 then 
@@ -48,26 +48,30 @@ return {
         local function UpdateSensor(sensor)
             TemperatureSensor = sensor.name
             sensor = string.gsub (sensor.name, "$Température ", "")
-            HumiditySensor = tostring("$Humidité "..sensor)
-            BarometreSensor = tostring("$Baromètre "..sensor)
-            if domoticz.devices(HumiditySensor).humidity and domoticz.devices(HumiditySensor).humidity ~= nil then
+            if domoticz.devices("$Humidité "..sensor) then HumiditySensor = tostring("$Humidité "..sensor) end
+            if domoticz.devices("$Baromètre "..sensor) then BarometreSensor = tostring("$Baromètre "..sensor) end
+            if domoticz.devices(HumiditySensor).humidity ~= nil and domoticz.devices(HumiditySensor).humidity then
                 h = domoticz.devices(HumiditySensor).humidity
                 hs = domoticz.devices(HumiditySensor).humidityStatusValue
-                logWrite(tostring(h), domoticz.LOG_DEBUG)
-                logWrite(tostring(hs), domoticz.LOG_DEBUG)
+                logWrite(h)
+                logWrite(hs)
             end
             t = domoticz.utils.round(domoticz.devices(TemperatureSensor).temperature,1)
-            logWrite(tostring(t), domoticz.LOG_DEBUG)
-            if domoticz.devices(BarometreSensor).barometer and domoticz.devices(BarometreSensor).barometer ~= nil then
+            logWrite(t)
+            if  domoticz.devices(BarometreSensor).barometer ~= nil and domoticz.devices(BarometreSensor).barometer then
                 p = tonumber(domoticz.devices(BarometreSensor).barometer)
                 fcst = domoticz.devices(BarometreSensor).forecastString
-                logWrite(tostring(p), domoticz.LOG_DEBUG)
-                logWrite(tostring(fcst), domoticz.LOG_DEBUG)
+                logWrite(p)
+                logWrite(fcst)
 
             end
             if h ~= nil and p ~= nil and domoticz.devices(sensor) then
                 domoticz.devices(sensor).updateTempHumBaro(t, h, hs, p, levelForecast(p))
-            end
+                logWrite("mise à jour du device "..domoticz.devices(sensor).name)
+           elseif h ~= nil and p == nil and domoticz.devices(sensor) then
+                domoticz.devices(sensor).updateTempHum(t, h, hs)
+                logWrite("mise à jour du device "..domoticz.devices(sensor).name)
+           end
         end
 
         UpdateSensor(sensor)
