c5b14644af2d01389bb952c9205c0dd4823e63e4
papo-o <papo-o@users.noreply.github.com>
2019-08-15 23:05:22 +0200

    v0.1 stopCommand.lua

--- dzVents/scripts/stopCommand.lua
+++ dzVents/scripts/stopCommand.lua
@@ -3,7 +3,7 @@ stopCommand.lua
 author/auteur = papoo
 update/mise à jour = 15/08/2019
 création = 15/08/2019
-https://pon.fr/
+https://pon.fr/dzvents-bouton-stop-sur-telecommande-de-volet-2-boutons
 https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/stopCommand.lua
 https://easydomoticz.com/forum/
 
