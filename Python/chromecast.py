6ffeb1b3de9d63dce14ece085a7b5467f0ea0bc1
papo-o <papo-o@users.noreply.github.com>
2019-09-15 11:15:25 +0200

    Update chromecast.py

--- Python/chromecast.py
+++ Python/chromecast.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from __future__ import print_function
-import sys; sys.path.insert(0,'/usr/local/lib/python3.4/dist-packages/')
+import sys; sys.path.insert(0,'/usr/local/lib/python3.6/dist-packages/')
 import time
-import pychromecast #https://github.com/balloob/pychromecast/tree/master/pychromecast
-from gtts import gTTS #https://github.com/pndurette/gTTS
+import pychromecast #https://github.com/balloob/pychromecast/tree/master/pychromecast  sudo pip3 install pychromecast --upgrade
+from gtts import gTTS #https://github.com/pndurette/gTTS  sudo pip3 install gTTS --upgrade
 #import sys
-URL_DOMOTICZ = 'http://127.0.0.1:8080/' # renseigner l'adresse et le port de votre domoticz
+URL_DOMOTICZ = 'http://192.168.100.248:8080/' # renseigner l'adresse et le port de votre domoticz
 for arg in sys.argv:
 	print(arg)
 tts = gTTS(text=arg, lang='fr', slow=False)
@@ -14,9 +14,9 @@ tts.save("/home/pi/domoticz/www/notification.mp3")
 chromecasts = pychromecast.get_chromecasts()
 
 [cc.device.friendly_name for cc in chromecasts]
-['Yamaha'] # mettre le nom de votre chromecast separe par unevirgule ex: ['douche', 'salon', 'cuisine', 'chambre']
+['Salon','Bureau'] # mettre le nom de votre chromecast séparé par une virgule ex: ['douche', 'salon', 'cuisine', 'chambre'] 
 	
-cast = next(cc for cc in chromecasts )
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == "Salon" )
 
 cast.wait()
 
@@ -27,5 +27,19 @@ mc.play_media(URL_DOMOTICZ+'notification.mp3', 'audio/mp3')
 mc.block_until_active()
 
 mc.pause()
-#time.sleep(1)
+time.sleep(1)
+mc.play()
+
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == "Bureau" )
+
+cast.wait()
+
+mc = cast.media_controller
+
+mc.play_media(URL_DOMOTICZ+'notification.mp3', 'audio/mp3')
+
+mc.block_until_active()
+
+mc.pause()
+time.sleep(1)
 mc.play()
