e9040ee088d97e6f5e4baef044b110d662c97963
papo-o <papo-o@users.noreply.github.com>
2019-12-27 22:32:44 +0100

    v1.02 daysSelectorIcons.lua

--- dzVents/scripts/daysSelectorIcons.lua
+++ dzVents/scripts/daysSelectorIcons.lua
@@ -3,10 +3,10 @@ source : https://www.domoticz.com/forum/viewtopic.php?f=59&t=27501&p=210819&hili
 https://pon.fr/dzvents-afficher-le-jour-de-la-semaine-sous-forme-d-icone/
 https://easydomoticz.com/forum/
 icones : https://github.com/papo-o/domoticz_scripts/tree/master/Icons/Days
- update/mise à jour = 29/07/2019
+ update/mise à jour = 27/12/2019
 --]]
 local scriptName        = 'daysSelectorIcons'
-local scriptVersion     = ' 1.0'
+local scriptVersion     = ' 1.02'
 return  {   
         active = true,
         on =    {  
@@ -14,16 +14,16 @@ return  {
                     },
 
    logging =   {  level    =   domoticz.LOG_DEBUG,                                             -- Seulement un niveau peut être actif; commenter les autres
-                -- -- level    =   domoticz.LOG_INFO,                                            -- Only one level can be active; comment others
-                -- -- level    =   domoticz.LOG_ERROR,
-                -- -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
+                -- -- -- level    =   domoticz.LOG_INFO,                                            -- Only one level can be active; comment others
+                -- -- -- level    =   domoticz.LOG_ERROR,
+                -- -- -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
                  marker = scriptName..' v'..scriptVersion
     },
 
-    execute = function(domoticz, item)
+    execute = function(dz, item)
     
         local function logWrite(str,level)
-            domoticz.log(tostring(str),level or domoticz.LOG_DEBUG)
+            dz.log(tostring(str),level or dz.LOG_DEBUG)
         end
         local icons =     { 
                             [10] = 143,-- level correspondant à dimanche
@@ -35,20 +35,11 @@ return  {
                             [70] = 142,-- level correspondant à samedi
 
                         }
-        
-        local function setIcon(iconNumber) 
-            local url = domoticz.settings['Domoticz url'] .. '/json.htm?type=setused&used=true&name=' .. domoticz.utils.urlEncode(item.name) ..
-            '&description=' .. domoticz.utils.urlEncode(item.description) .. -- Required. If not set it will be blanked out.
-            '&idx=' .. item.id .. 
-            '&switchtype=' .. item.switchTypeValue ..
-            '&customimage=' .. iconNumber
-            logWrite(url)
-            return domoticz.openURL(url)
-        end    
+           
         logWrite('lastLevel '.. tostring(item.lastLevel))
         logWrite('level '.. tostring(item.level))
         if item.level ~= item.lastLevel then 
-            setIcon(icons[item.level])
+            item.setIcon(icons[item.level])
         else
             logWrite('No Icon change necessary for ' .. item.id)
         end
