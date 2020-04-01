b08102cfd03c613da61d6511ae4e923ecef05324
papo-o <papo-o@users.noreply.github.com>
2020-04-01 13:22:01 +0200

    boiteAuxLettres.lua v2.01

--- dzVents/scripts/boiteAuxLettres.lua
+++ dzVents/scripts/boiteAuxLettres.lua
@@ -3,14 +3,14 @@ Creation =28/03/2018
 https://pon.fr/
 https://easydomoticz.com/forum/
 icones : https://github.com/papo-o/domoticz_scripts/boiteAuxLettres.lua
-update/mise à jour = 30/03/2020
+update/mise à jour = 01/04/2020
 Principe : Remettre à Zero les indicateurs paquet ou lettre
 --]]
 local scriptName        = 'Boite Aux Lettres'
-local scriptVersion     = ' 2.00'
+local scriptVersion     = ' 2.01'
 local message
 local subject               = '\xE2\x9A\xA0 /!\\ Le courrier n\'a pas été ramassé /!\\ \xE2\x9A\xA0'           -- sujet des notifications
-local subsystem             = 'telegram,HTTP'        -- les différentes valeurs de subsystem acceptées sont : gcm,http,kodi,lms,nma,prowl,pushalot,pushbullet,pushover,pushsafer,telegram
+local subsystem             = 'telegram,HTTP'   -- les différentes valeurs de subsystem acceptées sont : gcm,http,kodi,lms,nma,prowl,pushalot,pushbullet,pushover,pushsafer,telegram
                                                 -- pour plusieurs modes de notification séparez chaque mode par une virgule (exemple : "pushalot,pushbullet"). si subsystem = nil toutes les notifications seront activées.
 
 --------------------------------------------
@@ -24,15 +24,16 @@ return {
         devices =   { 'Paquet' }
     },
 
-    logging =   {  level    =   domoticz.LOG_DEBUG,           -- Seulement un niveau peut être actif; commenter les autres
-        -- -- level    =   domoticz.LOG_INFO,                 -- Only one level can be active; comment others
-        -- -- level    =   domoticz.LOG_ERROR,
-        -- -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
+    logging =
+    {   level    =   domoticz.LOG_DEBUG,                -- Seulement un niveau peut être actif; commenter les autres
+        -- level    =   domoticz.LOG_INFO,              -- Only one level can be active; comment others
+        -- level    =   domoticz.LOG_ERROR,
+        -- level    =   domoticz.LOG_MODULE_EXEC_INFO,
         marker = scriptName..' v'..scriptVersion
     },
 
-    execute = function(domoticz, item)
-    
+    execute = function(dz, item)
+
         local function logWrite(str,level)
             dz.log(tostring(str),level or dz.LOG_DEBUG)
         end
@@ -54,7 +55,7 @@ return {
         end
         return (split(str,','))
         end
-        
+
         if (item.isTimer) then
          -- the timer was triggered
         local message = nil
@@ -66,15 +67,15 @@ return {
         if message then
             dz.helpers.managedNotify(dz, subject, message, notificationTable(subsystem), nil , nil)
         end
-        
+
         elseif (item.isDevice and item.active) then
          -- it must be the detector
-        logWrite(mySwitch.name.. " est à " ..mySwitch.state)
+        logWrite(item.name.. " est à " ..item.state)
         logWrite(dz.devices('Lettre').name.." est à " ..dz.devices('Lettre').state)
-            if mySwitch.lastUpdate.hoursAgo < 8 then 
+            if item.lastUpdate.hoursAgo < 8 then
             --Si la porte a déjà été ouverte dans la journée on remet à off lettre et paquet
                 dz.devices('Lettre').switchOff().checkFirst()
-                mySwitch.switchOff().checkFirst()
+                item.switchOff().checkFirst()
             end
         end
     end
