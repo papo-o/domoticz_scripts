8dc8776bbab5517a7bdfaa38a99c5b24cdf564db
papo-o <papo-o@users.noreply.github.com>
2020-04-06 11:09:02 +0200

    xiaomiMP3.lua v2.01

--- dzVents/scripts/xiaomiMP3.lua
+++ dzVents/scripts/xiaomiMP3.lua
@@ -1,13 +1,13 @@
---[[XiaomiMP3.lua
+--[[xiaomiMP3.lua
 source = https://www.domoticz.com/wiki/Xiaomi_Gateway_(Aqara)
 https://pon.fr/
 https://easydomoticz.com/forum/
 github : https://github.com/papo-o/domoticz_scripts/xiaomiMP3.lua
-update/mise à jour = 30/03/2020
+update/mise à jour = 06/04/2020
 
 --]]
 local scriptName        = 'Xiaomi MP3'
-local scriptVersion     = ' 2.00'
+local scriptVersion     = ' 2.01'
 
 return {
     active  = true,
@@ -15,7 +15,7 @@ return {
         devices =   { 'Mid Value' }
     },
 
-     logging =   
+    logging =
     {   level    =   domoticz.LOG_DEBUG,           -- Seulement un niveau peut être actif; commenter les autres
         -- level    =   domoticz.LOG_INFO,         -- Only one level can be active; comment others
         -- level    =   domoticz.LOG_ERROR,
@@ -23,13 +23,13 @@ return {
         marker = scriptName..' v'..scriptVersion
     },
 
-    execute = function(domoticz, item)
+    execute = function(dz, item)
     
         local function logWrite(str,level)
             dz.log(tostring(str),level or dz.LOG_DEBUG)
         end
 
-        Calcvalue = (tonumber(item.svalues) + 10000)
+        Calcvalue = (tonumber(item.level) + 10000)
         dz.variables('XiaomiMP3').set( Calcvalue )
         logWrite('Xiaomi Gateway jouera le son stocké dans la banque '..Calcvalue)
         dz.devices('Xiaomi Gateway MP3').switchOn()
