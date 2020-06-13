e3d39303dba051053ec50b9171304f3e6ebe6613
papo-o <papo-o@users.noreply.github.com>
2020-06-13 21:38:02 +0200

    V2.01  vigilanceMeteoFrance.lua

--- dzVents/scripts/vigilanceMeteoFrance.lua
+++ dzVents/scripts/vigilanceMeteoFrance.lua
@@ -11,21 +11,21 @@ Une vigilance peut ne pas être associée à  un risque. dans ce cas, affichage 
 
 URL forum : http://easydomoticz.com/forum/viewtopic.php?f=17&t=5492
 URL blog : http://pon.fr/vigilance-meteofrance-v2/
-URL github : https://github.com/papo-o/domoticz_scripts/blob/master/Lua/script_time_vigilance_meteofrance_v2.lua
+URL github : https://github.com/papo-o/domoticz_scripts/blob/master/dzVents/scripts/vigilanceMeteoFrance.lua
 
 
 --]]
 --------------------------------------------
 ------------ Variables à éditer ------------
 --------------------------------------------
-local departement              = 87                   -- renseigner votre numéro de département sur 2 chiffres exemples : 01 ou 07 ou 87 
+local departement              = 26                   -- renseigner votre numéro de département sur 2 chiffres exemples : 01 ou 07 ou 87 
 local alert_device             = 'Vigilance Météo'    -- renseigner le nom de l'éventuel device alert vigilance météo associé (dummy - alert)
 
 --------------------------------------------
 ----------- Fin variables à éditer ---------
 --------------------------------------------
 local scriptName        = 'Vigilance météo France'
-local scriptVersion     = '2.0'
+local scriptVersion     = '2.01'
 local response = "vigilance_meteoFrance"
 return {
     active = true,
@@ -79,24 +79,28 @@ return {
             local abr = dz.utils.fromXML(item.data,"erreurXML") 
             local dv = abr.CV.DV
             local textAlert = ""
-            local vigilanceColor
+            local vigilanceColor = ""
 
             for i, departements in ipairs(dv) do
                 for _, result in pairs(departements) do
-                    logWrite(result.dep)
-                    logWrite(result.coul)
+                    -- logWrite(result.dep)
+                    -- logWrite(result.coul)
                     if tonumber(result.dep) == departement then 
                         logWrite("departement : "..result.dep)
                         logWrite("Couleur vigilance : "..result.coul)
                         vigilanceColor = tonumber(result.coul)
                         if dv[i].risque then 
                             risques = dv[i].risque
-                            for _, risques in pairs(risques) do 
-                                for _, risque in pairs(risques) do
-                                    if risque.val then 
-                                        logWrite(risqueTxt(tonumber(risque.val)))
-                                        
-                                        textAlert = textAlert .. risqueTxt(tonumber(risque.val)) .. ", "
+                            for _, risque in pairs(risques) do 
+                                for _, risqueDep in pairs(risque) do
+                                -- print_table(risqueDep)
+                                    if risqueDep.val and (type(risqueDep) == "table") then 
+                                        logWrite(risqueDep.val)
+                                        logWrite(risqueTxt(tonumber(risqueDep.val)))
+                                        textAlert = textAlert .. risqueTxt(tonumber(risqueDep.val)) .. ", "
+                                    else
+                                        logWrite(risqueDep)
+                                        textAlert = risqueTxt(tonumber(risqueDep))
                                     end
                                 end
                             end
@@ -105,7 +109,7 @@ return {
                 end
             end
             text = EnumClear(textAlert)
-            logWrite("vigilance ".. vigilanceColor .. text .. " pour le département " .. departement,dz.LOG_INFO)
+            logWrite("vigilance ".. vigilanceColor .. " " .. text .. " pour le département " .. departement,dz.LOG_INFO)
 
             if alert_device ~= nil then
                 if devAlert.color ~= vigilanceColor or devAlert.lastUpdate.minutesAgo > 1440 then
